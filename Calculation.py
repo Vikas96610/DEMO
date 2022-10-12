bb35ff3d68c1fc738e207550c6728a77c0f5e643
Vikas96610 <vikasrekha5586@gmail.com>
2022-10-12 13:45:48 +0530

    addition of 2 nums

---
+++ Calculation.py
@@ -0,0 +1,8 @@
+def add (x , y):       #defn of a functn add with 2 args x and y 
+    result = x + y      # value of x + y is stored in variable result
+    print(result)       #printing x + y
+    
+# additon of n1 and n2  
+n1 = int(input("enter x "))         # taking input from user as x. Converting this to int and storing it in variable n1
+n2 = int(input("ënter y "))         #taking input from user as y. Converting this to int and storing it in variable n2
+add(n1 , n2)               #calling the functn add along with passing n1 and n2 as 2 argument(args) 
